c5ea86075a78714a8b17894bb629edb8f579c77d
Norman Gilmore <git@teamforecast.com>
2020-10-28 12:49:48 -0700

    Update paths in database install script to location that is not clobbered on updates.

--- install_db_from_s3.sh
+++ install_db_from_s3.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+# $1 is the name of the database file to copy from S3.
 # The Elastic Beanstalk instance has permission to read, but not write,
 # to s3://research-ready/db-snapshots/
-aws s3 cp s3://research-ready/db-snapshots/2020-09-22-from-Keagan database.db
+aws s3 cp s3://research-ready/db-snapshots/${1} database.db
 # sudo is needed when logged in as ec2-user.
-sudo mkdir -p /var/app/current/static/db/new_09_17_2020/
+sudo mkdir -p /var/app/research-ready/
 # Use the path specified in config.py
-sudo mv database.db /var/app/current/static/db/new_09_17_2020/
+sudo mv database.db /var/app/research-ready/
 # Change new directory and database to be owned by webapp user:group
-sudo chown -R webapp:webapp /var/app/current/static/db/
+sudo chown -R webapp:webapp /var/app/research-ready/
 # Take a look and see if everything is in order.
-ls -l /var/app/current/static/db/new_09_17_2020/
+ls -l /var/app/research-ready/
